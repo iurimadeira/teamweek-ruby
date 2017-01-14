d43f1de4b5171e77a7af12c8c4ec55d80d416c8a
Iuri Madeira <iurimadeira@gmail.com>
2017-01-14 19:06:12 -0200

    Implements Group and GroupMembership's tests

--- spec/lib/teamweek/api/features/group_api_interaction_spec.rb
+++ spec/lib/teamweek/api/features/group_api_interaction_spec.rb
@@ -3,33 +3,31 @@ require "spec_helper"
 
 describe 'Group API interactions', Teamweek::Api::Client, :vcr do
 
-  xit "retrieves all groups" do
+  it "retrieves all groups" do
     all_groups = ["First user group", "New group"]
     groups = client.fetch(Teamweek::Api::Group)
     expect(groups.map(&:name)).to match_array all_groups
   end
 
-  xit "retrieves a specific group" do
-    group = client.fetch(Teamweek::Api::Group, {id: 175052})
-    expect(group.map(&:name)).to match_array ["First user group"]
+  it "retrieves a specific group" do
+    group = client.fetch(Teamweek::Api::Group, {id: 175548})
+    expect(group.map(&:name)).to match_array ["Group test"]
   end
 
-  xit "updates a group" do
-    group_before = client.fetch(Teamweek::Api::Group, {id: 175052})
-    group_after = client.update(Teamweek::Api::Group, {id: 175052, name: "Renamed group"})
-    expect(group_before.first.name).to eq "First user Group"
+  it "updates a group" do
+    group_after = client.update(Teamweek::Api::Group, {id: 175531, name: "Renamed group"})
     expect(group_after.first.name).to eq "Renamed group"
   end
 
-  xit "removes a group" do
-    group_before = client.fetch(Teamweek::Api::Group, {id: 175052})
-    client.delete(Teamweek::Api::Group, {id: 175052})
-    group_after = client.fetch(Teamweek::Api::Group, {id: 175052})
-    expect(group_before.first.name).to eq "First user group"
-    expect(group_after.first.name).to be_nil
+  it "removes a group" do
+    groups_before = client.fetch(Teamweek::Api::Group)
+    client.delete(Teamweek::Api::Group, {id: 175547})
+    groups_after = client.fetch(Teamweek::Api::Group)
+    expect(groups_before.map(&:id)).to include 175547
+    expect(groups_after.map(&:id)).to_not include 175547
   end
 
-  xit "creates a group" do
+  it "creates a group" do
     Timecop.freeze Date.new(2017, 1, 20) do
       group = client.create(Teamweek::Api::Group, {name: "New group", date: Time.now.iso8601})
       expect(group.first.name).to eq "New group"
--- spec/lib/teamweek/api/features/group_membership_api_interaction_spec.rb
+++ spec/lib/teamweek/api/features/group_membership_api_interaction_spec.rb
@@ -3,30 +3,30 @@ require "spec_helper"
 
 describe 'GroupMembership API interactions', Teamweek::Api::Client, :vcr do
 
-  xit "retrieves all group_memberships from a group" do
-    all_group_memberships = []
-    group_memberships = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175052})
-    expect(group_memberships.map(&:user_id)).to match_array all_group_memberships
+  it "retrieves all group_memberships from a group" do
+    all_group_memberships = [599779, 599780, 599781]
+    group_memberships = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175548})
+    expect(group_memberships.map(&:id)).to match_array all_group_memberships
   end
 
-  xit "updates a group_membership" do
-    group_membership = client.update(Teamweek::Api::GroupMembership, {group_id: 175052, id: 598083, user_id: 1054512})
-    expect(group_membership.first.user_id).to eq 1054512
+  it "updates a group_membership" do
+    group_membership = client.update(Teamweek::Api::GroupMembership, {group_id: 175548, id: 599781, weight: 10})
+    expect(group_membership.first.weight).to eq 10
   end
 
-  xit "removes a group_membership" do
-    group_memberships_before = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175052})
-    client.delete(Teamweek::Api::GroupMembership, {id: 598083})
-    group_memberships_after = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175052})
+  it "removes a group_membership" do
+    group_memberships_before = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175548})
+    client.delete(Teamweek::Api::GroupMembership, {group_id: 175548, id: 599782})
+    group_memberships_after = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175548})
 
-    expect(group_memberships_before.map(&:user_id)).to include ""
-    expect(group_memberships_before.map(&:user_id)).to_not include ""
+    expect(group_memberships_before.map(&:id)).to include 599782
+    expect(group_memberships_after.map(&:id)).to_not include 599782
   end
 
-  xit "creates a group_membership" do
+  it "creates a group_membership" do
     Timecop.freeze Date.new(2017, 1, 20) do
-      group_membership = client.create(Teamweek::Api::GroupMembership, {group_id: 175052,  user_id: 1054512})
-      expect(group_membership.first.user_id).to eq 175052
+      group_membership = client.create(Teamweek::Api::GroupMembership, {group_id: 175548, user_id: 1054513})
+      expect(group_membership.first.user_id).to eq 1054513
     end
   end
 end
