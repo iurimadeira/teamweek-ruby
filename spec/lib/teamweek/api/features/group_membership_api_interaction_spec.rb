8af63ec038e7308e93e0a703314d4789f518d694
Iuri Madeira <iurimadeira@gmail.com>
2017-01-14 15:00:07 -0200

    Marks all Group and GroupMembership's specs as pending

--- spec/lib/teamweek/api/features/group_membership_api_interaction_spec.rb
+++ spec/lib/teamweek/api/features/group_membership_api_interaction_spec.rb
@@ -3,18 +3,18 @@ require "spec_helper"
 
 describe 'GroupMembership API interactions', Teamweek::Api::Client, :vcr do
 
-  it "retrieves all group_memberships from a group" do
+  xit "retrieves all group_memberships from a group" do
     all_group_memberships = []
     group_memberships = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175052})
     expect(group_memberships.map(&:user_id)).to match_array all_group_memberships
   end
 
-  it "updates a group_membership" do
+  xit "updates a group_membership" do
     group_membership = client.update(Teamweek::Api::GroupMembership, {group_id: 175052, id: 598083, user_id: 1054512})
     expect(group_membership.first.user_id).to eq 1054512
   end
 
-  it "removes a group_membership" do
+  xit "removes a group_membership" do
     group_memberships_before = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175052})
     client.delete(Teamweek::Api::GroupMembership, {id: 598083})
     group_memberships_after = client.fetch(Teamweek::Api::GroupMembership, {group_id: 175052})
@@ -23,7 +23,7 @@ describe 'GroupMembership API interactions', Teamweek::Api::Client, :vcr do
     expect(group_memberships_before.map(&:user_id)).to_not include ""
   end
 
-  it "creates a group_membership" do
+  xit "creates a group_membership" do
     Timecop.freeze Date.new(2017, 1, 20) do
       group_membership = client.create(Teamweek::Api::GroupMembership, {group_id: 175052,  user_id: 1054512})
       expect(group_membership.first.user_id).to eq 175052
